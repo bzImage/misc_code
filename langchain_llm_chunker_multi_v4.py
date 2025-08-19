c333e971e943b98ac598352c4f22897ff5e01a68
Adrian de los Santos <55768963+bzImage@users.noreply.github.com>
2025-08-19 08:03:35 -0600

    Update langchain_llm_chunker_multi_v4.py

--- langchain_llm_chunker_multi_v4.py
+++ langchain_llm_chunker_multi_v4.py
@@ -3,7 +3,7 @@
 # Demon - bzImage
 # 
 """
-Markdown with embedded images (docling output)-> Extract and saave Images -> Pre‑chunk -> Semantic merge -> LLM chunking -> FAISS
+Markdown with embedded images (docling output)-> Extract/indentify and save Images -> Pre‑chunk -> Semantic merge -> LLM chunking -> FAISS
 
 Stages / artifacts:
   0) read_md                           -> in‑memory
